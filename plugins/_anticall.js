d9029db700b0c6383b60b7bea924b12d1eda1837
Satriaop <satriamahadmadanir@gmail.com>
2025-03-03 15:43:24 +0700

    by zakkiXD-Dev

---
+++ plugins/_anticall.js
@@ -0,0 +1,19 @@
+let handler = m => m
+
+handler.before = async function (m) {
+  this.ev.on('call', async (call) => {
+      if (call[0].status == 'offer') {
+       
+        await this.sessage(call[0].from, {
+          text: "",
+          quoted: call[0]
+        });
+        await this.rejectCall(call[0].id, call[0].from);
+        await this.updateBlockStatus(call[0].from, "block");
+      }
+  });
+}
+
+module.exports = handler
+
+//admin mohon maaf atas kesalahan kode sebelum nya
