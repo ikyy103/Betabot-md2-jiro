664f4c73cff7d0e906c086d68b5e0888431a2aed
Satriaop <satriamahadmadanir@gmail.com>
2025-03-03 15:32:59 +0700

    by zakkiXD-Dev

---
+++ plugins/kerang-bisakah.js
@@ -0,0 +1,16 @@
+let handler = async (m, { conn, command, text }) => {
+  if (!text) return m.reply("Masukan Text!");
+  let caption = `
+*🌎Pertanyaan:* ${command} ${text}
+*💬Jawaban:* ${pickRandom(["Iya", "Bisa", "Tentu saja bisa", "Tentu bisa", "Sudah pasti", "Sudah pasti bisa", "Tidak", "Tidak bisa", "Tentu tidak", "tentu tidak bisa", "Sudah pasti tidak"])}
+`.trim();
+  m.reply(caption, false, { mentions: await conn.parseMention(caption) });
+};
+handler.help = ["bisakah"];
+handler.tags = ["kerang"];
+handler.command = /^bisakah/i;
+module.exports = handler;
+
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)];
+}
