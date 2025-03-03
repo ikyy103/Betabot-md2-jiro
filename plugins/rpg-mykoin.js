5a2410134cf973b69048cd823bc80020c6596aaf
Satriaop <satriamahadmadanir@gmail.com>
2025-03-03 15:42:37 +0700

    by zakkiXD-Dev

---
+++ plugins/rpg-mykoin.js
@@ -0,0 +1,23 @@
+let handler = async (m, { conn, command }) => {
+  let user = global.db.data.users[m.sender];
+  if (!user.koin) {
+    return conn.reply(m.chat, 'Kamu belum memiliki koin. Waktunya untuk deposit!', m);
+  }
+  const caption = `*▸ Nama:* ${user.registered ? user.name : conn.getName(m.sender)}
+*▸ Limit:* ${user.premiumTime >= 1 ? 'Unlimited' : user.limit}
+*▸ Koin:* ${user.koin}
+
+*Tanggal:* ${new Date().toLocaleDateString('id-ID')}
+*Jam:* ${new Date().toLocaleTimeString('id-ID')}
+`.trim();
+  
+  await conn.reply(m.chat, caption, m);
+}
+
+handler.help = ['mykoin'];
+handler.tags = ['judi','rpg'];
+handler.command = /^(mykoin)$/i;
+
+handler.register = false;
+
+module.exports = handler;
