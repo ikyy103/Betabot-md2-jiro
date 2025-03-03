420634e577052d16edcbc57835716736d487bdd5
Satriaop <satriamahadmadanir@gmail.com>
2025-03-03 15:40:59 +0700

    by zakkiXD-Dev

---
+++ plugins/pay-config.js
@@ -0,0 +1,15 @@
+export const paymentConfig = {
+    email: 'satriamahadmadanir@gmail.com',
+    userID: 'Jirodonate',
+    name: 'Zephyr',
+    prices: {
+        3: 5000,      // 3 Hari
+        7: 10000,      // 1 Minggu
+        14: 12000,    // 2 Minggu
+        21: 14000,    // 3 Minggu
+        30: 15000,    // 1 Bulan
+        60: 30000,    // 2 Bulan
+        90: 60000,    // 3 Bulan
+        365: 120000,   // 1 Tahun
+    },
+};
