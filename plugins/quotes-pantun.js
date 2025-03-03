5f0425601b71039dc52fca1afed5fada1e400cdc
Satriaop <satriamahadmadanir@gmail.com>
2025-03-03 15:32:06 +0700

    by zakkiXD-Dev

---
+++ plugins/quotes-pantun.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+let handler = async (m) => {
+  let url = await axios.get(
+    "https://raw.githubusercontent.com/IdkJhus/JhuszBotV1/main/node_modules/ra-api/lib/database/pantun.json",
+  );
+  let data = url.data.getRandom();
+  m.reply(data.trim());
+};
+handler.help = ["pantun"];
+handler.tags = ["quotes"];
+handler.command = /^(pantun)$/i;
+handler.limit = true;
+
+module.exports = handler;
