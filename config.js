615541e59e1e4e2f1a90aca70c67611fdf31853e
Satriaop <satriamahadmadanir@gmail.com>
2025-03-13 12:52:09 +0700

    Replace file config.js

--- config.js
+++ config.js
@@ -1,43 +1,57 @@
-global.owner = ['6281547205987']  
-global.mods = ['6281547205987'] 
-global.prems = ['6281547205987']
-global.nameowner = 'Zephyr'
-global.numberowner = '6281547205987'
-global.mail = 'satriahubdeals@gmail.com' 
-global.gc = 'https://chat.whatsapp.com/DkTQfKV6sza2W503p1sOMx'
-global.instagram = 'https://instagram.com/satriaonly2024'
-global.wm = '© Jiro'
-global.wait = '_*Tunggu sedang di proses...*_'
-global.eror = '_*Server Error*_'
-global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
-global.packname = 'Made With'
-global.author = 'Bot WhatsApp'
-global.maxwarn = '2' // Peringatan maksimum
-global.antiporn = true // Auto delete pesan porno (bot harus admin)
+global.owner = ['6281547205987'];
+global.mods = ['6281547205987'];
+global.prems = ['6281547205987'];
 
-//INI WAJIB DI ISI!//
-global.lann = 'isi_apikey' 
-//Daftar terlebih dahulu https://api.betabotz.eu.org
+global.nameowner = 'Zephyr';
+global.numberowner = '6281547205987';
+global.mail = 'satriahubdeals@gmail.com';
+global.gc = 'https://chat.whatsapp.com/DkTQfKV6sza2W503p1sOMx';
+global.instagram = 'https://instagram.com/satriaonly2024';
+global.wm = '© Jiro';
+global.wait = '_*Tunggu sedang di proses...*_';
+global.eror = '_*Server Error*_';
+global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*';
+global.packname = 'Made With';
+global.author = 'Bot WhatsApp';
+global.maxwarn = '2'; // Peringatan maksimum
+global.antiporn = true; // Auto delete pesan porno (bot harus admin)
 
-//INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA//
-global.btc = 'isi_apikey'
-//Daftar https://api.botcahx.eu.org 
+// INI WAJIB DI ISI! //
+global.lann = 'isi_apikey'; 
+// Daftar terlebih dahulu di https://api.betabotz.eu.org
+
+// INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA //
+global.btc = 'isi_apikey';
+// Daftar di https://api.botcahx.eu.org 
+
+// Ini bebas isi apa ga //
+global.maelyn = 'isi_apikey';
+// Daftar di https://api.maelyn.tech
+
+// ini bebas mau di isi apa ga //
+global.fastrestapis = 'isi_apikey';
+
 
 global.APIs = {   
   lann: 'https://api.betabotz.eu.org',
-  btc: 'https://api.botcahx.eu.org'
-}
+  btc: 'https://api.botcahx.eu.org',
+  maelyn: 'https://api.maelyn.tech',
+  fastrestapis: 'https://fastrestapis.fasturl.cloud'
+};
+
 global.APIKeys = { 
   'https://api.betabotz.eu.org': global.lann, 
-  'https://api.botcahx.eu.org': global.btc
-}
+  'https://api.botcahx.eu.org': global.btc,
+  'https://api.maelyn.tech': global.maelyn,
+  'https://fastrestapis.fasturl.cloud': global.fastrestapis
+};
 
-let fs = require('fs')
-let chalk = require('chalk')
-let file = require.resolve(__filename)
+let fs = require('fs');
+let chalk = require('chalk');
+let file = require.resolve(__filename);
 fs.watchFile(file, () => {
-  fs.unwatchFile(file)
-  console.log(chalk.redBright("Update 'config.js'"))
-  delete require.cache[file]
-  require(file)
-})
+  fs.unwatchFile(file);
+  console.log(chalk.redBright("Update 'config.js'"));
+  delete require.cache[file];
+  require(file);
+});
