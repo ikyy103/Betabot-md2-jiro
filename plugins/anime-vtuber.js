4b70a7facc6eb08d620851b9368b112c6ee0f372
Satriaop <satriamahadmadanir@gmail.com>
2025-03-23 04:22:48 +0700

    Replace file anime-vtuber.js

--- plugins/anime-vtuber.js
+++ plugins/anime-vtuber.js
@@ -1,21 +1,20 @@
-import { wiki } from "vtuber-wiki"
+const { wiki } = require("vtuber-wiki");
 
-const handler = async (m, { conn, text }) =>
-{
+const handler = async (m, { conn, text }) => {
+    if (!text) return m.reply(`Example .vtuber kobo kanaeru`);
 
-if (!text) return m.reply(`Example .vtuber kobo kanaeru`)
-
-async function getVTuber(vtuber) {
-    try {
-        const result = await wiki(vtuber);
-        if (!result.image_url) return { error: "No such vTuber" };
-        return result;
-    } catch (err) {
-        return { error: "No such vTuber" };
+    async function getVTuber(vtuber) {
+        try {
+            const result = await wiki(vtuber);
+            if (!result.image_url) return { error: "No such vTuber" };
+            return result;
+        } catch (err) {
+            return { error: "No such vTuber" };
+        }
     }
-}
-let tuber = await getVTuber(text)
-let pituber = `*[ VTUBER WIKI ]*
+
+    let tuber = await getVTuber(text);
+    let pituber = `*[ VTUBER WIKI ]*
 
 *\`Judul:\`* ${tuber.title} 
 *\`Link:\`* ${tuber.url}
@@ -25,18 +24,20 @@ let pituber = `*[ VTUBER WIKI ]*
 *\`Type:\`* ${tuber.type}
 *\`Channel:\`* ${tuber.channel}
 *\`Social Media:\`* ${tuber.social_media}
-*\`Offcial Website:\`* ${tuber.official_website}
+*\`Official Website:\`* ${tuber.official_website}
 *\`Gender:\`* ${tuber.gender}
 *\`Age:\`* ${tuber.age}
 *\`Description:\` ${tuber.description}
 *\`More:\`* ${tuber.more}
-`
+`;
 
-await conn.sendMessage(m.chat, {image: {url: tuber.image_url}, caption: pituber}, {quoted: m})
-}
-handler.command = ['vtuber', 'virtualyoutuber']
-handler.help = ['vtuber']
-handler.tags = ['anime']
+    await conn.sendMessage(m.chat, { image: { url: tuber.image_url }, caption: pituber }, { quoted: m });
+};
+
+handler.command = ['vtuber', 'virtualyoutuber'];
+handler.help = ['vtuber'];
+handler.tags = ['anime'];
 handler.error = 0;
 handler.limit = true;
+
 module.exports = handler;
