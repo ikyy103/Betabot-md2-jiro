efc6802ed718895ccc55f242d9573c19e2e4ea30
Satriaop <satriamahadmadanir@gmail.com>
2025-03-30 11:36:48 +0700

    Replace file downloader-videy.js

--- plugins/downloader-videy.js
+++ plugins/downloader-videy.js
@@ -1,20 +1,32 @@
 const axios = require('axios');
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `Masukan URL!\n\ncontoh:\n${usedPrefix + command} https://videy.co/v?id=QtZ8jT1X1`;    
-    try {
-        if (!text.match(/videy/gi)) throw `URL Tidak Ditemukan!`;        
-        m.reply(wait);      
-        let res = await axios.get(`https://api.tioo.eu.org/download/videy?url=${text}`)
-        let data = res.data.result
-        await conn.sendFile(m.chat, data, 'videy.mp4', "*DONE*", m);      
-    } catch (e) {
-        console.log(e);
-        throw e
+  if (!text) throw `Masukan URL!\n\ncontoh:\n${usedPrefix + command} https://videy.co/v?id=QtZ8jT1X1`;
+
+  try {
+    if (!text.match(/videy/gi)) throw `URL Tidak Ditemukan!`;
+
+    m.reply('Tunggu sebentar...');
+
+    const apikey = '${global.lann}';
+    const url = `https://api.betabotz.eu.org/api/download/videy?url=${text}&apikey=${lann}`;
+
+    const res = await axios.get(url);
+    const data = res.data;
+
+    if (data.status) {
+      await conn.sendFile(m.chat, data.result, 'videy.mp4', '*DONE*', m);
+    } else {
+      throw data.message;
     }
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
 };
+
 handler.help = ['videy'];
-handler.command = /^(videy|videydl)$/i
+handler.command = /^(videy|videydl)$/i;
 handler.tags = ['downloader'];
 handler.limit = true;
 
