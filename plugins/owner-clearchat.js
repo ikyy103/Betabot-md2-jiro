92ed5812133c2193daa20c5875795c3932ccdf45
Satriaop <satriamahadmadanir@gmail.com>
2025-04-11 12:26:22 +0700

    Replace file owner-clearchat.js

--- plugins/owner-clearchat.js
+++ plugins/owner-clearchat.js
@@ -1,19 +1,11 @@
-let handler = async (m, { conn, args }) => {
-  if (!args[0]) throw 'Masukkan nomor atau JID! Contoh: .clearchat 6281234567890';
-  
-  let jid = args[0].includes('@s.whatsapp.net') ? args[0] : `${args[0]}@s.whatsapp.net`;
-  
-  try {
-    await conn.chatModify({ clear: { messages: [{ id: m.key.id, fromMe: false, timestamp: m.messageTimestamp }] } }, jid, [])
-    await conn.sendMessage(m.chat, { text: `Berhasil menghapus chat dengan ${jid}` }, { quoted: m })
-  } catch (e) {
-    m.reply('Gagal menghapus chat. Pastikan nomor tersebut valid atau ada dalam daftar chat.');
-  }
-};
+let handler = async (m, { conn }) => {
+  conn.chatModify({ delete: true, lastMessages: [{ key: m.key, messageTimestamp: m.messageTimestamp }] }, m.chat)
+  let a = await m.reply("Successfully deleted this chat!")
+}
 
-handler.help = ['clearchat <nomor>'];
+handler.help = ['deletechat'];
 handler.tags = ['owner'];
-handler.command = /^clearchat$/i;
+handler.command = /^(deletechat|delchat|dchat|clearchat|cleanchat)$/i;
 handler.owner = true;
 
 module.exports = handler;
