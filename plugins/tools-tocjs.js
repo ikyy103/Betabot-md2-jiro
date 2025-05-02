fe04af2f70ed4dd27a92cce8806d3c77c8c073c0
Satriaop <satriamahadmadanir@gmail.com>
2025-05-02 16:10:11 +0700

    Replace file tools-tocjs.js

--- plugins/tools-tocjs.js
+++ plugins/tools-tocjs.js
@@ -1,45 +1,62 @@
-let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-    let codeToConvert = text || (m.quoted && m.quoted.text);
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  const q = m.quoted || m
+  const mime = (q.msg || q).mimetype || ''
 
-    if (!codeToConvert) throw `Masukkan atau reply kode yang ingin diubah`;
+  let codeToConvert = text
 
-    let result;
-    if (command === 'toesm') {
-        result = convertCJSToESM(codeToConvert);
-    } else if (command === 'tocjs') {
-        result = convertESMToCJS(codeToConvert);
+  if (!text && q.fileSha256) {
+    // Kalau file
+    let buffer = await q.download()
+    let contentType = mime.split('/')[0]
+
+    // Kalau file teks, langsung baca string-nya
+    if (contentType === 'text' || mime.includes('json') || mime.includes('javascript')) {
+      codeToConvert = buffer.toString()
     } else {
-        throw `Perintah tidak dikenal`;
+      return m.reply(`File bertipe *${mime}* tidak bisa diproses untuk konversi kode.`)
     }
+  }
 
-    m.reply(result);
-};
+  if (!codeToConvert) throw `Masukkan teks atau reply file/teks berisi kode yang ingin diubah.`
 
-handler.help = ['toesm <kode>', 'tocjs <kode>'];
-handler.tags = ['code'];
-handler.command = /^(toesm|tocjs)$/i;
-handler.limit = true;
+  let result
+  if (command === 'toesm') {
+    result = convertCJSToESM(codeToConvert)
+  } else if (command === 'tocjs') {
+    result = convertESMToCJS(codeToConvert)
+  } else {
+    throw `Perintah tidak dikenal`
+  }
 
-module.exports = handler;
-
-function convertCJSToESM(code) {
-    return code
-        .replace(/const (\w+) = require\(['"](.+?)['"]\);?/g, 'import $1 from \'$2\';')
-        .replace(/let (\w+) = require\(['"](.+?)['"]\);?/g, 'import $1 from \'$2\';')
-        .replace(/var (\w+) = require\(['"](.+?)['"]\);?/g, 'import $1 from \'$2\';')
-        .replace(/module\.exports\s*=\s*({?.*}?);?/g, 'export default $1;')
-        .replace(/exports\.(\w+)\s*=\s*(\w+);?/g, 'export const $1 = $2;');
+  m.reply(`Hasil konversi:\n\n${result}`)
 }
 
+handler.help = ['toesm <kode>', 'tocjs <kode> (bisa reply file teks)']
+handler.tags = ['tools']
+handler.command = /^(toesm|tocjs)$/i
+handler.limit = true
 
+module.exports = handler
+
+// Fungsi convert CJS ke ESM
+function convertCJSToESM(code) {
+  return code
+    .replace(/const (\w+) = require['"](.+?)['"];?/g, 'import $1 from \'$2\';')
+    .replace(/let (\w+) = require['"](.+?)['"];?/g, 'import $1 from \'$2\';')
+    .replace(/var (\w+) = require['"](.+?)['"];?/g, 'import $1 from \'$2\';')
+    .replace(/module\.exports\s*=\s*({?.*}?);?/g, 'export default $1;')
+    .replace(/exports\.(\w+)\s*=\s*(\w+);?/g, 'export const $1 = $2;')
+}
+
+// Fungsi convert ESM ke CJS
 function convertESMToCJS(code) {
-    return code
-        .replace(/import (\w+) from ['"](.+?)['"];/g, 'const $1 = require(\'$2\');')
-        .replace(/import \* as (\w+) from ['"](.+?)['"];/g, 'const $1 = require(\'$2\');')
-        .replace(/import \{(.*?)\} from ['"](.+?)['"];/g, (match, p1, p2) => {
-            const imports = p1.split(',').map(i => i.trim());
-            return `const { ${imports.join(', ')} } = require('${p2}');`;
-        })
-        .replace(/export default (\w+);?/g, 'module.exports = $1;')
-        .replace(/export const (\w+) = (\w+);?/g, 'exports.$1 = $2;');
+  return code
+    .replace(/import (\w+) from ['"](.+?)['"];/g, 'const $1 = require(\'$2\');')
+    .replace(/import \* as (\w+) from ['"](.+?)['"];/g, 'const $1 = require(\'$2\');')
+    .replace(/import \{(.*?)\} from ['"](.+?)['"];/g, (match, p1, p2) => {
+      const imports = p1.split(',').map(i => i.trim())
+      return `const { ${imports.join(', ')} } = require('${p2}');`
+    })
+    .replace(/export default (\w+);?/g, 'module.exports = $1;')
+    .replace(/export const (\w+) = (\w+);?/g, 'exports.$1 = $2;')
 }
